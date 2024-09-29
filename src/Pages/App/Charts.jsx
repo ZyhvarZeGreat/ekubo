b9c28e7b25b33a114dd694bfe159f6f284e7409a
Bolude Daniel <98927610+ZyhvarZeGreat@users.noreply.github.com>
2024-09-29 16:25:34 +0100

    initial commit

---
+++ src/Pages/App/Charts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+const Charts = () => {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Charts
